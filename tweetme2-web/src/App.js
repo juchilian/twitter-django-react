2f9c5a25f0bde6f4cee7763be3796ad94023cb12
juchilian <juchilian@outlook.jp>
2021-02-08 18:19:10 +0900

    62. Pass from Parent Component to Child

--- tweetme2-web/src/App.js
+++ tweetme2-web/src/App.js
@@ -1,52 +1,10 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
-function loadTweets(callback) {
-  const xhr = new XMLHttpRequest();
-  const method = "GET"; // "POST"
-  const url = "http://localhost:8000/api/tweets/";
-  const responseType = "json";
-  xhr.responseType = responseType;
-  xhr.open(method, url);
-  xhr.onload = function () {
-    callback(xhr.response, xhr.status);
-  };
-  xhr.onerror = function (e) {
-    console.log(e);
-    callback({ message: "The request was an error" }, 400);
-  };
-  xhr.send();
-}
-
-function Tweet(props) {
-  const { tweet } = props;
-  const className = props.className
-    ? props.className
-    : "col-10 mx-auto col-md-6";
-  return (
-    <div className={className}>
-      <p>
-        {tweet.id} - {tweet.content}
-      </p>
-    </div>
-  );
-}
+import { TweetsComponent } from "./tweets";
 
 function App() {
-  const [tweets, setTweets] = useState([]);
-
-  useEffect(() => {
-    const myCallback = (response, status) => {
-      console.log(response, status);
-      if (status === 200) {
-        setTweets(response);
-      } else {
-        alert("There was an error");
-      }
-    };
-    loadTweets(myCallback);
-  }, []);
   return (
     <div className="App">
       <header className="App-header">
@@ -55,15 +13,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <div>
-          {tweets.map((item, index) => {
-            return (
-              <Tweet
-                tweet={item}
-                className="my-5 py-5 border bg-white text-dark"
-                key={`${index}-{item.id}`}
-              />
-            );
-          })}
+          <TweetsComponent />
         </div>
         <a
           className="App-link"
