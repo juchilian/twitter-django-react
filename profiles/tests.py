44ab2fe988b3ce060c2af8d33f9ff65cbb2664bf
juchilian <juchilian@outlook.jp>
2021-02-11 08:30:25 +0900

    90. Followers and Following

--- profiles/tests.py
+++ profiles/tests.py
@@ -1,3 +1,32 @@
 from django.test import TestCase
 
 # Create your tests here.
+from django.contrib.auth import get_user_model
+from .models import Profile
+
+
+User = get_user_model()
+
+
+class ProfileTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username='cfe', password='somepassword')
+        self.userb = User.objects.create_user(
+            username='cfe-2', password='somepassword2')
+
+    def test_profile_created_via_signal(self):
+        qs = Profile.objects.all()
+        self.assertEqual(qs.count(), 2)
+
+    def test_following(self):
+        first = self.user
+        second = self.userb
+        first.profile.followers.add(second)  # added a follower
+        second_user_following_whom = second.following.all()
+        # from a user, check other user is being followed.
+        qs = second_user_following_whom.filter(user=first)
+        # check new user has is not following anyone
+        first_user_following_no_one = first.following.all()
+        self.assertTrue(qs.exists())
+        self.assertFalse(first_user_following_no_one.exists())
