da546c9bed653b3112c18cfbaee588c3c6c49a79
juchilian <juchilian@outlook.jp>
2021-02-10 18:49:45 +0900

    88. Save 2 Models in 1 Form and 1 View

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login, logout
+from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 # Create your views here.
 
 # Function based views to Class Based Views
+
+
 def login_view(request, *args, **kwargs):
     form = AuthenticationForm(request, data=request.POST or None)
     if form.is_valid():
@@ -14,8 +16,9 @@ def login_view(request, *args, **kwargs):
         "form": form,
         "btn_label": "Login",
         "title": "Login"
-        }
+    }
     return render(request, "accounts/auth.html", context)
+
 
 def logout_view(request, *args, **kwargs):
     if request.method == "POST":
@@ -29,14 +32,15 @@ def logout_view(request, *args, **kwargs):
     }
     return render(request, "accounts/auth.html", context)
 
+
 def register_view(request, *args, **kwargs):
     form = UserCreationForm(request.POST or None)
     if form.is_valid():
         user = form.save(commit=True)
         user.set_password(form.cleaned_data.get("password1"))
         # send a confirmation email to verify their account
-        print(form.cleaned_data)
-        # username = form.cleaned_data.get("username")
+        login(request, user)
+        return redirect("/")
     context = {
         "form": form,
         "btn_label": "Register",
