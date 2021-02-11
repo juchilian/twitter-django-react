9a2e727d881c5a481ff9be0d3be0cb6b0315daa0
juchilian <juchilian@outlook.jp>
2021-02-11 09:59:06 +0900

    94. More Efficient Backend Lookups

--- profiles/models.py
+++ profiles/models.py
@@ -4,10 +4,12 @@ from django.db.models.signals import post_save
 
 User = settings.AUTH_USER_MODEL
 
+
 class FollowerRelation(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     profile = models.ForeignKey("Profile", on_delete=models.CASCADE)
     timestamp = models.DateTimeField(auto_now_add=True)
+
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -16,7 +18,11 @@ class Profile(models.Model):
     timestamp = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     followers = models.ManyToManyField(User, related_name='following', blank=True)
-
+    """
+    project_obj = Profile.objects.first()
+    project_obj.followers.all() -> All users following this profile
+    user.following.all() -> All user profiles I follow
+    """
 
 
 def user_did_save(sender, instance, created, *args, **kwargs):
