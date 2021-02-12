6a326928997d3dad6375913a5a7f3f1f9f0c4b33
juchilian <juchilian@outlook.jp>
2021-02-12 16:50:20 +0900

    105. Render Profile Badge Component

--- tweetme2-web/src/index.js
+++ tweetme2-web/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { FeedComponent, TweetsComponent, TweetDetailComponent } from "./tweets";
+import { ProfileBadgeComponent } from "./profiles";
 import * as serviceWorker from "./serviceWorker";
 
 const appEl = document.getElementById("root");
@@ -24,6 +25,12 @@ const tweetDetailElements = document.querySelectorAll(".tweetme-2-detail");
 
 tweetDetailElements.forEach((container) => {
   ReactDOM.render(e(TweetDetailComponent, container.dataset), container);
+});
+
+const userProfileBadgeElements = document.querySelectorAll(".tweetme-2-profile-badge");
+
+userProfileBadgeElements.forEach((container) => {
+  ReactDOM.render(e(ProfileBadgeComponent, container.dataset), container);
 });
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- tweetme2-web/src/profiles/index.js
+++ tweetme2-web/src/profiles/index.js
@@ -1,3 +1,4 @@
 import { UserPicture, UserDisplay, UserLink } from "./components";
+import { ProfileBadgeComponent } from "./badge";
 
-export { UserPicture, UserDisplay, UserLink };
+export { ProfileBadgeComponent, UserPicture, UserDisplay, UserLink };
